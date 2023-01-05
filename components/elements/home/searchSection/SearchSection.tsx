45f44e68dc6a615878b384522477c9e83250c411
Marat <110041770+MaratBagautdinov@users.noreply.github.com>
2023-01-05 16:22:15 +0300

    Merge branch 'main' of C:\Users\ADMIN\Documents\GitHub\travel-next with conflicts.

---
+++ components/elements/home/searchSection/SearchSection.tsx
@@ -0,0 +1,17 @@
+import s from './SearchSection.module.css'
+import {FC} from "react";
+import Search from "./Search/Search";
+import Filters from "../../Filters/Filters";
+import mapImage from '../../../../images/world-map.png'
+
+const SearchSection:FC = () => {
+  return <section className={s.section} style={{backgroundImage: `url('${mapImage.src}')`}}>
+      <div>
+          <h1>Best places for trip</h1>
+          <Search/>
+          <Filters/>
+      </div>
+  </section>
+}
+
+export default SearchSection
